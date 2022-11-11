f619bea5861aed954791bd021ab8a8bf36f6ae84
jamesduong-byb <87284927+jamesduong-byb@users.noreply.github.com>
2022-11-11 11:58:37 +1100

    test import publish dry run

--- src/__tests__/components/ResponsiveTable.test.tsx
+++ src/__tests__/components/ResponsiveTable.test.tsx
@@ -2,6 +2,8 @@ import { render } from '@testing-library/react';
 import { ResponsiveTable } from '../../components/ResponsiveTable';
 import { responsiveTableData } from '../../test-props';
 
+const dog = '';
+
 describe('ResponsiveTable', () => {
   test('renders component', () => {
     render(<ResponsiveTable data={responsiveTableData} />);
