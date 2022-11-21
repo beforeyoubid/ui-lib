81856a6ce2d629ae305e708cf728ff207110805f
jamesduong-byb <87284927+jamesduong-byb@users.noreply.github.com>
2022-11-21 14:48:26 +1100

    feat: add automation constants and props to textfield

--- src/utils.ts
+++ src/utils.ts
@@ -22,3 +22,33 @@ export const createOrder = () => {
 };
 
 export const isEvenNum = (num: number) => num % 2 === 0;
+
+export const notEmpty = <TValue>(value: TValue | null | undefined): value is TValue =>
+  value !== null && value !== undefined;
+
+export function automation(componentKeys: Maybe<string>[] | string, attributes = {}) {
+  const dataAttributes: Record<string, unknown> = {};
+  if (Array.isArray(componentKeys)) {
+    componentKeys.filter(notEmpty).forEach(key => {
+      if (key.toLocaleLowerCase().includes('component.')) {
+        dataAttributes['data-component-key'] = key;
+      }
+      if (key.toLocaleLowerCase().includes('page')) {
+        dataAttributes['data-page-component-key'] = key;
+      }
+    });
+  } else if (componentKeys) {
+    if (componentKeys.toLocaleLowerCase().includes('component.')) {
+      dataAttributes['data-component-key'] = componentKeys;
+    }
+    if (componentKeys.toLocaleLowerCase().includes('page')) {
+      dataAttributes['data-page-component-key'] = componentKeys;
+    }
+  }
+  for (const [key, value] of Object.entries(attributes)) {
+    dataAttributes[`data-component-${key.toLocaleLowerCase()}`] = value;
+  }
+  return {
+    ...dataAttributes,
+  };
+}
