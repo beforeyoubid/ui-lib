e7dc200af836e4695f1452ab1ec5d083a9c767e0
Alice <alice@beforeyoubid.com.au>
2023-03-31 14:43:01 +1100

    removed unused variables / imports

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import { readFile } from 'fs/promises';
 import { terser } from 'rollup-plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import json from '@rollup/plugin-json';
@@ -9,8 +8,6 @@ import json from '@rollup/plugin-json';
 import glob from 'glob';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-
-const packageJson = JSON.parse(await readFile(new URL('./package.json', import.meta.url)));
 
 export default [
   {
