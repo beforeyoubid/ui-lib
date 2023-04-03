435a6c65bf8e5259d8493d2a680ed062d5445c93
Bailey Sheather <bailey@beforeyoubid.com.au>
2023-04-03 11:42:53 +1000

    added SWC for jest

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ const config = {
   testEnvironment: 'jsdom',
   collectCoverage: true,
   testPathIgnorePatterns: ['module_test'],
+  transform: {
+    '\\.(ts|tsx)$': '@swc/jest',
+  },
 };
 
 module.exports = config;
