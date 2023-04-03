ef136513ed0817cd566c244cac57289e176e466d
Alice <alice@beforeyoubid.com.au>
2023-04-03 17:09:57 +1000

    separated ESM / CJS test phases

---
+++ module_test/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { TextField } from '@beforeyoubid/ui-lib';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')!
+);
+const element = <TextField />;
+root.render(element);
