7d4f736c5130f096b13bbb2edcf1980c49c24ad3
Bailey Sheather <bailey@beforeyoubid.com.au>
2023-06-27 16:20:01 +1000

    fixed storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,13 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
   rules: {
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-unused-vars': ['error', {
+      argsIgnorePattern: '^_'
+    }],
     '@typescript-eslint/explicit-function-return-type': 0,
     '@typescript-eslint/camelcase': 0,
     '@typescript-eslint/interface-name-prefix': 0,
@@ -18,14 +20,12 @@ module.exports = {
     '@typescript-eslint/defined-types-are-used': 0,
     'prefer-const': 0,
     '@typescript-eslint/ban-ts-ignore': 'off',
-    '@typescript-eslint/no-unnecessary-type-constraint': 'off',
+    '@typescript-eslint/no-unnecessary-type-constraint': 'off'
   },
-  overrides: [
-    {
-      files: ['**/*.stories.*'],
-      rules: {
-        'import/no-anonymous-default-export': 'off',
-      },
-    },
-  ],
+  overrides: [{
+    files: ['**/*.stories.*'],
+    rules: {
+      'import/no-anonymous-default-export': 'off'
+    }
+  }]
 };
