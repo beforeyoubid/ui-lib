c119b932c0eeedadfdd56c836a5a9c236e77d4a4
Bailey Sheather <bailey@beforeyoubid.com.au>
2023-07-06 16:01:17 +1000

    moved typing around

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -1,29 +1,29 @@
 import { styled } from '@mui/material/styles';
-import { Color, TypographyFontClass, TypographyFontSize } from '../mui-theme';
+import { Color, TypographyFontSize, TypographyFontClass } from '../mui-theme';
 
 export type TypographyProps = {
   class: TypographyFontClass;
   size: TypographyFontSize;
   color: Color;
+  padding?: number;
   children: React.ReactNode;
 };
 
 export function Typography(props: TypographyProps) {
   return (
-    <Div fontClass={props.class} size={props.size} color={props.color}>
+    <Div fontClass={props.class} size={props.size} color={props.color} padding={props.padding ?? 1}>
       {props.children}
     </Div>
   );
 }
 
-const Div = styled('div')<{ fontClass: TypographyFontClass; size: TypographyFontSize; color: Color }>(
-  ({ theme, fontClass, size, color }) => ({
-    ...theme.typography.button,
+const Div = styled('div')<{ fontClass: TypographyFontClass; size: TypographyFontSize; color: Color; padding: number }>(
+  ({ theme, fontClass, size, color, padding }) => ({
     fontFamily: theme.typography.fontFamily,
     fontSize: theme.typography.size[size].fontSize,
     lineHeight: theme.typography.size[size].lineHeight,
     fontWeight: theme.typography.fontWeight[fontClass],
     color: theme.palette.colors[color],
-    padding: theme.spacing(1),
+    padding: theme.spacing(padding),
   })
 );
