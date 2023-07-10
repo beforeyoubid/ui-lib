944056421af759c70aa8b9d609cbbd1467960361
Alice <alice@beforeyoubid.com.au>
2023-07-10 13:59:48 +1000

    added height to flex wrapper

--- src/components/FlexWrapper.tsx
+++ src/components/FlexWrapper.tsx
@@ -11,6 +11,7 @@ const FlexWrapper = styled('div')<{
   textAlign?: CSSProperties['textAlign'];
   basis?: CSSProperties['flexBasis'];
   width?: CSSProperties['width'];
+  height?: CSSProperties['height'];
 }>`
   display: flex;
   flex-direction: ${props => props.direction ?? 'row'};
@@ -25,6 +26,11 @@ const FlexWrapper = styled('div')<{
     width &&
     css`
       width: ${formatSize(width)};
+    `}
+  ${({ height }) =>
+    height &&
+    css`
+      height: ${formatSize(height)};
     `}
 `;
 
