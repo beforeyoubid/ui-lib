234affa985551beec3d55bf3df61c587a6224cf2
Alice <alice@beforeyoubid.com.au>
2023-07-10 13:58:05 +1000

    cleaned responsive table component

--- src/components/ResponsiveTable/Table.tsx
+++ src/components/ResponsiveTable/Table.tsx
@@ -1,12 +1,14 @@
-import { Table, TableCell, TableRow, Paper, TableBody, TableContainer, TableHead } from '@mui/material';
+import { Table as MuiTable, TableCell, TableRow, Paper, TableBody, TableContainer, TableHead } from '@mui/material';
 
-import { MyTableProps } from './types';
+import { TableProps } from './types';
 
-export const MyTable = ({ data }: MyTableProps): JSX.Element => {
-  const renderHeaders = (): JSX.Element => {
-    const headers = Object.keys(data[0]);
-    return (
-      <>
+export const Table = ({ data }: TableProps): JSX.Element => {
+  const headers = Object.keys(data[0]);
+  const rows = Object.values(data[0]);
+
+  return (
+    <TableContainer component={Paper}>
+      <MuiTable style={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
             {headers.map(h => (
@@ -16,34 +18,17 @@ export const MyTable = ({ data }: MyTableProps): JSX.Element => {
             ))}
           </TableRow>
         </TableHead>
-      </>
-    );
-  };
-
-  const renderAllRows = (): JSX.Element => {
-    const rows = Object.values(data[0]);
-
-    return <TableBody>{renderOneRow(rows)}</TableBody>;
-  };
-
-  const renderOneRow = (rowValue: (string | number | JSX.Element)[]): JSX.Element => {
-    return (
-      <TableRow>
-        {rowValue.map((r, i) => (
-          <TableCell align="left" key={i}>
-            {r}
-          </TableCell>
-        ))}
-      </TableRow>
-    );
-  };
-
-  return (
-    <TableContainer component={Paper}>
-      <Table style={{ minWidth: 650 }} aria-label="simple table">
-        {renderHeaders()}
-        {renderAllRows()}
-      </Table>
+        <TableBody>
+          <TableRow>
+            {rows.map((r, i) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <TableCell align="left" key={i}>
+                {r}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableBody>
+      </MuiTable>
     </TableContainer>
   );
 };
