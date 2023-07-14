0b723ad043b879d21688d4a3921ae54da446cac6
Bailey Sheather <bailey@beforeyoubid.com.au>
2023-07-14 13:27:56 +1000

    fixed story

--- src/stories/PasswordInput.stories.tsx
+++ src/stories/PasswordInput.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import PasswordInput from '../components/PasswordInput';
+import { PasswordInput } from '../components/PasswordInput';
 const meta: Meta<typeof PasswordInput> = {
   component: PasswordInput,
 };
