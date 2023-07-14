bd4ce526cd74250452061e58cc42fb83a7a74127
Alice <alice@beforeyoubid.com.au>
2023-07-14 13:35:22 +1000

    added other components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,16 @@
 /*
   Try to keep exports in alphabetical order
 */
+export * from './Button';
 export * from './Icon';
 export * from './KebabMenu';
 export * from './KebabMenu/types';
-export * from './ResponsiveTable/types';
-export { ResponsiveTable } from './ResponsiveTable';
-export * from './ProfilePicture';
-export * from './TextField';
+export * from './MediaCard';
+export * from './NavigationButton';
+export * from './NavigationMenu';
 export * from './PasswordInput';
-export * from './Button';
+export * from './ProfilePicture';
+export * from './ResponsiveTable';
+export * from './ResponsiveTable/types';
+export * from './TextField';
+export * from './Typography';
