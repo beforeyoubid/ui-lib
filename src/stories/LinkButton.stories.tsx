090128a0d6fc2dda7150b48ff4e4004243c6c8f1
Ben Ojha <ben@beforeyoubid.com.au>
2023-07-17 16:21:58 +1000

    Initial commit

---
+++ src/stories/LinkButton.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { LinkButton } from '../components/LinkButton';
+
+const meta: Meta<typeof LinkButton> = {
+  component: LinkButton,
+};
+
+export default meta;
+type Story = StoryObj<typeof LinkButton>;
+
+export const Active: Story = {
+  args: {
+    type: 'mint',
+    title: 'Login',
+    size: 'lg',
+    disableRipple: true,
+  },
+};
