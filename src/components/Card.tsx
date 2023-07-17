3b95c66811d3a0f2ced9ebe5122fbfbc46ec1a1e
Alice <alice@beforeyoubid.com.au>
2023-07-17 15:21:48 +1000

    fixed Card padding

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -23,12 +23,13 @@ type CardButtonsProps = {
 export type CardProps = {
   title: string;
   children: React.ReactNode;
+  fullWidth?: boolean;
 } & (({ editable: true } & CardButtonsProps) | { editable: false });
 
 export function Card(props: CardProps) {
   const theme = useTheme();
   return (
-    <Wrapper direction="column" gap={theme.spacing(4)}>
+    <Wrapper direction="column" gap={theme.spacing(4)} fullWidth={props.fullWidth ?? true}>
       <Flex direction="row" width="100%" align="center">
         <Typography color="navy" class="bold" size="2xl" padding={0}>
           {props.title}
@@ -92,10 +93,12 @@ function CardButtonsNoMemo(props: CardButtonsProps) {
 
 const CardButtons = React.memo(CardButtonsNoMemo);
 
-const Wrapper = styled(Flex)(({ theme }) => ({
+const Wrapper = styled(Flex)<{ fullWidth: boolean }>(({ theme, fullWidth }) => ({
   outline: `1px solid ${theme.palette.colors.dark15}`,
   backgroundColor: theme.palette.colors.lightWhite,
   padding: theme.spacing(6, 5),
+  width: fullWidth ? `calc(100% - ${theme.spacing(5)})` : undefined,
+  maxWidth: `calc(100% - ${theme.spacing(10)})`,
 }));
 
 const ChildrenWrapper = styled('div')({
