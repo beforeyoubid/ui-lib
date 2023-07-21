3416269ccd542c19ced391fb8d329910a9907404
Bailey Sheather <bailey@beforeyoubid.com.au>
2023-07-21 13:37:27 +1000

    rearranged storybook story

--- src/stories/KebabMenu.stories.tsx
+++ src/stories/KebabMenu.stories.tsx
@@ -1,11 +1,9 @@
-import { Meta, Story } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 import { KebabMenu } from '../components/KebabMenu';
-import { KebabMenuProps } from '../components/KebabMenu/types';
-import { ThemedApp } from './styles';
 
-export default {
-  title: 'ui-lib/KebabMenu',
+const meta: Meta<typeof KebabMenu> = {
   component: KebabMenu,
+  title: 'Navigation/KebabMenu',
   argTypes: {
     size: {
       options: ['sm', 'md', 'lg'],
@@ -19,95 +17,94 @@ export default {
       table: { disable: true },
     },
   },
-} as Meta;
-
-const Template: Story<KebabMenuProps> = args => (
-  <ThemedApp>
-    <KebabMenu {...args} />
-  </ThemedApp>
-);
-
-export const Main = Template.bind({});
-Main.args = {
-  options: [
-    {
-      type: 'VIEW_ORDER',
-      label: 'View order',
-      icon: 'Eye',
-      description: 'See a detailed page for this order',
-    },
-    {
-      type: 'REQUEST_REPORT_UPDATE',
-      label: 'Request a report update',
-      icon: 'Reload',
-      description: 'Ask for a re-inspection or a free re-upload',
-    },
-    {
-      type: 'SELECT_SUCCESSFUL_BUYER',
-      label: 'Select successful buyer',
-      icon: 'UserCheck',
-    },
-    {
-      type: 'SEND_PURCHASE_LINK_LETTER',
-      label: 'Send purchase link letter',
-      icon: 'MailForward',
-      description: 'Share this with interested parties',
-    },
-  ],
 };
 
-export const ExtraOptions = Template.bind({});
-ExtraOptions.args = {
-  options: [
-    {
-      type: 'VIEW_ORDER',
-      label: 'View order',
-      icon: 'Eye',
-      description: 'See a detailed page for this order',
-    },
-    {
-      type: 'REQUEST_REPORT_UPDATE',
-      label: 'Request a report update',
-      icon: 'Reload',
-      description: 'Ask for a re-inspection or a free re-upload',
-    },
-    {
-      type: 'SELECT_SUCCESSFUL_BUYER',
-      label: 'Select successful buyer',
-      icon: 'UserCheck',
-    },
-    {
-      type: 'SEND_PURCHASE_LINK_LETTER',
-      label: 'Send purchase link letter',
-      icon: 'MailForward',
-      description: 'Share this with interested parties',
-    },
-    {
-      type: 'CONVERT_TO_BUYER_FREE',
-      label: 'Convert to buyer free',
-      icon: 'ArrowForward',
-    },
-    {
-      type: 'EDIT_ADDRESS',
-      label: 'Edit address',
-      icon: 'Edit',
-    },
-    {
-      type: 'HOLD_REPORT',
-      label: 'Put on hold',
-      icon: 'PlayerPause',
-      description: 'this will block re-purchases until resumed',
-    },
-    {
-      type: 'CLOSE_IN_2_DAYS',
-      label: 'Close in 2 days',
-      icon: 'SquareRoundedLetterX',
-    },
-    {
-      type: 'CLOSE_NOW',
-      label: 'Close now',
-      icon: 'Trash',
-      description: 'this will notify the linked users',
-    },
-  ],
+export default meta;
+type Story = StoryObj<typeof KebabMenu>;
+
+export const Main: Story = {
+  args: {
+    options: [
+      {
+        type: 'VIEW_ORDER',
+        label: 'View order',
+        icon: 'Eye',
+        description: 'See a detailed page for this order',
+      },
+      {
+        type: 'REQUEST_REPORT_UPDATE',
+        label: 'Request a report update',
+        icon: 'Reload',
+        description: 'Ask for a re-inspection or a free re-upload',
+      },
+      {
+        type: 'SELECT_SUCCESSFUL_BUYER',
+        label: 'Select successful buyer',
+        icon: 'UserCheck',
+      },
+      {
+        type: 'SEND_PURCHASE_LINK_LETTER',
+        label: 'Send purchase link letter',
+        icon: 'MailForward',
+        description: 'Share this with interested parties',
+      },
+    ],
+  },
+};
+
+export const ExtraOptions: Story = {
+  args: {
+    options: [
+      {
+        type: 'VIEW_ORDER',
+        label: 'View order',
+        icon: 'Eye',
+        description: 'See a detailed page for this order',
+      },
+      {
+        type: 'REQUEST_REPORT_UPDATE',
+        label: 'Request a report update',
+        icon: 'Reload',
+        description: 'Ask for a re-inspection or a free re-upload',
+      },
+      {
+        type: 'SELECT_SUCCESSFUL_BUYER',
+        label: 'Select successful buyer',
+        icon: 'UserCheck',
+      },
+      {
+        type: 'SEND_PURCHASE_LINK_LETTER',
+        label: 'Send purchase link letter',
+        icon: 'MailForward',
+        description: 'Share this with interested parties',
+      },
+      {
+        type: 'CONVERT_TO_BUYER_FREE',
+        label: 'Convert to buyer free',
+        icon: 'ArrowForward',
+      },
+      {
+        type: 'EDIT_ADDRESS',
+        label: 'Edit address',
+        icon: 'Edit',
+      },
+      {
+        type: 'HOLD_REPORT',
+        label: 'Put on hold',
+        icon: 'PlayerPause',
+        description: 'this will block re-purchases until resumed',
+      },
+      {
+        type: 'CLOSE_IN_2_DAYS',
+        label: 'Close in 2 days',
+        icon: 'SquareRoundedLetterX',
+      },
+      {
+        type: 'CLOSE_NOW',
+        label: 'Close now',
+        icon: 'Trash',
+        description: 'this will notify the linked users',
+      },
+    ],
+  },
 };
