d79da953fe56bc32890c82931e8a4a06f3aa8626
akram@beforeyoubid.com.au <akram@beforeyoubid.com.au>
2023-08-07 10:45:26 +1000

    fix character counting

--- src/components/TextArea/index.tsx
+++ src/components/TextArea/index.tsx
@@ -11,7 +11,7 @@ export type TextAreaProps = TextInputProps &
   };
 
 export const TextArea = (props: TextAreaProps) => {
-  const characterCount = props.value?.trim().length ?? 0;
+  const characterCount = props.value?.length ?? 0;
 
   const handleTextChange = useCallback(
     (event: React.ChangeEvent<HTMLTextAreaElement>) => {
