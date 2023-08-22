225e9a1cbaeaebe667d61136bfc4143e04030dff
Alice Sheather <alice@beforeyoubid.com.au>
2023-08-22 12:48:44 +1000

    added modal test

---
+++ src/__tests__/components/Modal.test.tsx
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react';
+import { Modal } from '../../components/Modal';
+import { ThemedApp } from '../../stories/styles';
+
+describe('Modal', () => {
+  test('renders component', () => {
+    render(
+      <ThemedApp>
+        <Modal title="Some title" open onClose={console.log}>
+          Some content
+        </Modal>
+      </ThemedApp>
+    );
+  });
+});
