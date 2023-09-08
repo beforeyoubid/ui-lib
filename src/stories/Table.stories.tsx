5a33cf50e93f30428eff1300c6c4bf65b0af1f75
Alice Sheather <alice@beforeyoubid.com.au>
2023-09-08 13:02:31 +1000

    added strikethrough to table row

--- src/stories/Table.stories.tsx
+++ src/stories/Table.stories.tsx
@@ -56,3 +56,34 @@ export const Main: Story = {
     ),
   },
 };
+
+export const Strikethrough: Story = {
+  args: {
+    children: (
+      <Table>
+        <TableHeader>
+          <TableCell>Dessert (100g serving)</TableCell>
+          <TableCell align="right">Calories</TableCell>
+          <TableCell align="right" sort sortDirection="asc" onSort={console.log} sortKey="fat" isBeingSorted>
+            Fat&nbsp;(g)
+          </TableCell>
+          <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+          <TableCell align="right">Protein&nbsp;(g)</TableCell>
+        </TableHeader>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow key={row.name} strikethrough={row.name === 'Eclair'}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    ),
+  },
+};
