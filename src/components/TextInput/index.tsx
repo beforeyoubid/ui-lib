10d1b20e484ddafeae10a45d0a2619646d5c28db
Alice Sheather <alice@beforeyoubid.com.au>
2023-09-11 15:12:10 +1000

    added support for start & end adornments + spaced them correctly

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,11 +1,12 @@
-import { InputAdornment, StandardTextFieldProps, useTheme } from '@mui/material';
+import { StandardTextFieldProps, useTheme } from '@mui/material';
 
-import { CustomTextField } from './styles';
+import { CustomTextField, StyledInputAdornment } from './styles';
 import { TextFieldErrorLabel, TextFieldHint, TextFieldLabel } from './Labels';
 import { Icon, IconProps } from '../Icon';
 import { Flex } from '../Flex';
 
 import { automation } from '../../utils';
+import { Typography } from '../Typography';
 
 export type TextInputProps = {
   label: string;
@@ -17,6 +18,8 @@ export type TextInputProps = {
   required?: boolean;
   isOptional?: boolean;
   automationKey?: string;
+  startAdornment?: string | React.ReactNode;
+  endAdornment?: string | React.ReactNode;
 } & Omit<StandardTextFieldProps, 'required' | 'variant' | 'helperText'>;
 
 export const TextInput = (props: TextInputProps) => {
@@ -31,6 +34,8 @@ export const TextInput = (props: TextInputProps) => {
     required = false,
     isOptional = false,
     automationKey,
+    startAdornment,
+    endAdornment,
     ...rest
   } = props;
 
@@ -47,12 +52,10 @@ export const TextInput = (props: TextInputProps) => {
         color="primary"
         value={value}
         fullWidth
+        hasLeadingIcon={!!leadingIconName}
         InputProps={{
-          startAdornment: leadingIconName ? (
-            <InputAdornment position="start">
-              <Icon icon={leadingIconName} color="dark75" />
-            </InputAdornment>
-          ) : null,
+          startAdornment: <Adornment position="start" adornment={startAdornment} icon={leadingIconName} />,
+          endAdornment: <Adornment position="end" adornment={endAdornment} />,
         }}
         inputProps={{
           ...automation([automationKey], { label }),
@@ -60,5 +63,37 @@ export const TextInput = (props: TextInputProps) => {
         {...rest}
       />
     </Flex>
+  );
+};
+
+const Adornment = ({
+  adornment,
+  position,
+  icon,
+}: {
+  adornment?: string | React.ReactNode;
+  position: 'start' | 'end';
+  icon?: TextInputProps['leadingIconName'];
+}) => {
+  if (adornment && icon) {
+    throw new Error('cannot have both adornment and leadingIconName');
+  }
+  if (icon) {
+    return (
+      <StyledInputAdornment position={position} icon className="icon-adornment">
+        <Icon icon={icon} color="dark75" size="18" />
+      </StyledInputAdornment>
+    );
+  }
+  if (!adornment) return null;
+  if (!(typeof adornment === 'string')) {
+    return <StyledInputAdornment position={position}>{adornment}</StyledInputAdornment>;
+  }
+  return (
+    <StyledInputAdornment position={position}>
+      <Typography color="dark60" class="medium" size="base">
+        {adornment}
+      </Typography>
+    </StyledInputAdornment>
   );
 };
