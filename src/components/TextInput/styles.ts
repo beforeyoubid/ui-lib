902e81530f57b0f075e97e70a7d1dd4d7129504c
Alice Sheather <alice@beforeyoubid.com.au>
2023-09-11 15:12:10 +1000

    added support for start & end adornments + spaced them correctly

--- src/components/TextInput/styles.ts
+++ src/components/TextInput/styles.ts
@@ -1,4 +1,4 @@
-import { TextField, styled } from '@mui/material';
+import { InputAdornment, TextField, styled } from '@mui/material';
 import { Flex } from '../Flex';
 
 export const HelperTextErrorWrapper = styled(Flex)(({ theme }) => ({
@@ -24,7 +24,7 @@ export const LabelRowContainer = styled('div')<{ required: boolean }>(({ theme, 
   gap: theme.spacing(0.25),
 }));
 
-export const CustomTextField = styled(TextField)`
+export const CustomTextField = styled(TextField)<{ hasLeadingIcon: boolean }>`
   & .MuiOutlinedInput-root {
     border-color: ${({ error, disabled, theme }) =>
       error ? theme.palette.colors.error60 : disabled ? theme.palette.colors.error15 : theme.palette.colors.dark45};
@@ -51,12 +51,27 @@ export const CustomTextField = styled(TextField)`
     }
   }
 
-  & .MuiOutlinedInput-startAdornment {
-    position: absolute;
-    left: 0;
-    top: 50%;
-    transform: translateY(-50%);
+  & .MuiInputAdornment-positionStart.MuiInputAdornment-outlined {
     pointer-events: none;
+    &:not(.icon-adornment) {
+      margin-left: 0;
+      margin-right: 0;
+      padding: ${({ theme }) => theme.spacing(0, 1, 0, 1.75)};
+    }
+  }
+  & .MuiInputAdornment-positionEnd.MuiInputAdornment-outlined {
+    pointer-events: none;
+    &:not(.icon-adornment) {
+      margin-left: 0;
+      margin-right: 0;
+      padding: ${({ theme }) => theme.spacing(0, 1.75)};
+    }
+  }
+  & .icon-adornment {
+    margin-right: 0;
+    margin-left: 0;
+
+    padding: ${({ theme }) => theme.spacing(0, 0, 0, 1.75)};
   }
   & .MuiOutlinedInput-root {
     padding: 0px;
@@ -64,5 +79,32 @@ export const CustomTextField = styled(TextField)`
       padding: ${({ theme }) => theme.spacing(1.5, 1.75)};
       ${({ multiline }) => !multiline && `height: 21px;`}
     }
+  }
+  & .MuiInputBase-adornedStart .MuiOutlinedInput-input {
+    padding-left: ${({ theme, hasLeadingIcon }) => theme.spacing(hasLeadingIcon ? 0.5 : 1.25)};
+  }
+
+  .Mui-focused .MuiInputAdornment-outlined {
+    :before,
+    :after {
+      background-color: ${({ error, disabled, theme }) =>
+        disabled ? theme.palette.colors.dark45 : error ? theme.palette.colors.error60 : theme.palette.colors.mint60};
+    }
+  }
+`;
+
+export const StyledInputAdornment = styled(InputAdornment)<{ icon?: boolean }>`
+  position: relative;
+  padding: ${({ theme }) => theme.spacing(0, 1, 0, 1.75)};
+
+  ${({ position }) => (position === 'start' ? ':after' : ':before')} {
+    position: absolute;
+    content: ${({ icon }) => (icon ? 'unset' : `''`)};
+    height: 45px;
+    background-color: ${({ theme }) => theme.palette.colors.dark45};
+    width: 1px;
+    display: block;
+    ${({ position }) => (position === 'start' ? 'right' : 'left')}: 0;
+    top: -${({ theme }) => theme.spacing(2.75)};
   }
 `;
