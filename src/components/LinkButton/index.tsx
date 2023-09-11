052679155c66e80c09e113fa96c3846a0156da65
Alice Sheather <alice@beforeyoubid.com.au>
2023-09-11 15:11:33 +1000

    added leading icon prop to LinkButton

--- src/components/LinkButton/index.tsx
+++ src/components/LinkButton/index.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
-import { styled, Button } from '@mui/material';
+import { styled, Button, useTheme } from '@mui/material';
 import { ButtonProps as MuiButtonProps } from '@mui/material';
 import { Typography } from '../Typography';
 import { useLinkButtonFontStyle, getLinkButtonStyle } from './utils';
 import { Colors } from '../../mui-theme';
+import { Flex } from '../Flex';
+import { Icon, IconProps } from '../Icon';
 
 export type LinkButtonProps = {
   type: 'mint' | 'white' | 'grey' | 'red';
   title: string;
   size: 'lg' | 'md' | 'sm';
   underline?: boolean;
+  leadingIcon?: IconProps['icon'];
 } & Omit<MuiButtonProps, 'variant' | 'children' | 'color' | 'type' | 'size'>;
 
-export const LinkButton: React.FC<LinkButtonProps> = ({ type, title, size, disabled, ...rest }) => {
+export const LinkButton: React.FC<LinkButtonProps> = ({
+  type,
+  title,
+  size,
+  disabled,
   underline,
+  leadingIcon,
+  ...rest
+}) => {
+  const theme = useTheme();
   const linkButtonStyle = getLinkButtonStyle(disabled ? 'grey' : type);
   const linkFontStyle = useLinkButtonFontStyle(size);
   return (
@@ -27,14 +38,17 @@ export const LinkButton: React.FC<LinkButtonProps> = ({ type, title, size, disab
       underline={underline ?? true}
       {...rest}
     >
-      <Typography
-        class={linkFontStyle.fontClass}
-        size={linkFontStyle.fontSize}
-        color={linkButtonStyle.BgColor}
-        hoverColor={linkButtonStyle.hoverBgColor}
-      >
-        {title}
-      </Typography>
+      <Flex gap={theme.spacing(1.25)} align="center">
+        {leadingIcon && <Icon icon={leadingIcon} color={linkButtonStyle.BgColor} size="18" />}
+        <Typography
+          class={linkFontStyle.fontClass}
+          size={linkFontStyle.fontSize}
+          color={linkButtonStyle.BgColor}
+          hoverColor={linkButtonStyle.hoverBgColor}
+        >
+          {title}
+        </Typography>
+      </Flex>
     </ButtonWrapper>
   );
 };
