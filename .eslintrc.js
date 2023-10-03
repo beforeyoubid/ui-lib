8d5c7a55250598b38edce88d35810538803e733a
Alice Sheather <alice@beforeyoubid.com.au>
2023-10-03 11:35:48 +1100

    fixed eslint warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,11 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
