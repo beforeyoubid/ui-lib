4770ca425a18d355d1c04273e99650016b46ece3
akram@beforeyoubid.com.au <akram@beforeyoubid.com.au>
2024-03-19 12:46:36 +1100

    refactor component

--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -2,7 +2,6 @@ import type React from 'react';
 import { useCallback, useState } from 'react';
 
 import { type DateView } from '@mui/x-date-pickers';
-import dayjs, { type Dayjs } from 'dayjs';
 
 import { Flex } from '../Flex';
 import { Icon } from '../Icon';
@@ -14,22 +13,29 @@ import { StyledDatePicker } from './styles';
 
 type DatePickerProps = {
   label: string;
-  initialDate?: string;
-  onChange: (date: string) => void;
+  date: unknown;
+  dateMonth: string;
+  dateYear: string;
+  incrementMonth: () => void;
+  decrementMonth: () => void;
+  incrementYear: () => void;
+  decrementYear: () => void;
+  onChange: (value: unknown) => void;
 };
 
-export const DatePicker: React.FC<DatePickerProps> = ({ label, initialDate, onChange }) => {
+export const DatePicker: React.FC<DatePickerProps> = ({
+  label,
+  date,
+  dateMonth,
+  dateYear,
+  incrementMonth,
+  decrementMonth,
+  incrementYear,
+  decrementYear,
+  onChange,
+}) => {
   const [isOpen, setIsOpen] = useState(false);
   const [currentView, setCurrentView] = useState<DateView>('day');
-  const [selectedDate, setSelectedDate] = useState<Dayjs>(dayjs(initialDate));
-
-  const goToPreviousMonth = useCallback(() => setSelectedDate(curr => curr.subtract(1, 'month').set('date', 1)), []);
-
-  const goToNextMonth = useCallback(() => setSelectedDate(curr => curr.add(1, 'month').set('date', 1)), []);
-
-  const goToPreviousYear = useCallback(() => setSelectedDate(curr => curr.subtract(1, 'year').set('date', 1)), []);
-
-  const goToNextYear = useCallback(() => setSelectedDate(curr => curr.add(1, 'year').set('date', 1)), []);
 
   const toggleMonthView = useCallback(() => {
     setCurrentView(currView => (currView === 'month' ? 'day' : 'month'));
@@ -49,19 +55,13 @@ export const DatePicker: React.FC<DatePickerProps> = ({ label, initialDate, onCh
         {label}
       </Typography>
       <StyledDatePicker
-        value={selectedDate}
+        value={date}
         openTo={currentView}
         open={isOpen}
         format="MMMM D, YYYY"
         views={['year', 'month', 'day']}
         onClose={() => setCurrentView('day')}
-        onChange={(value: unknown) => {
-          if (!(value as any)?.['$isDayjsObject']) return;
-
-          const date = value as Dayjs;
-          setSelectedDate(date);
-          onChange(date.format('DD-MM-YYYY'));
-        }}
+        onChange={onChange}
         slots={{
           layout: props => <CalendarFooter toggleCalendar={toggleOpen}>{props.children}</CalendarFooter>,
           openPickerIcon: () => (
@@ -71,12 +71,12 @@ export const DatePicker: React.FC<DatePickerProps> = ({ label, initialDate, onCh
           ),
           calendarHeader: () => (
             <CalendarHeader
-              selectedMonth={selectedDate.format('MMM')}
-              selectedYear={selectedDate.format('YYYY')}
-              goToPreviousMonth={goToPreviousMonth}
-              goToNextMonth={goToNextMonth}
-              goToPreviousYear={goToPreviousYear}
-              goToNextYear={goToNextYear}
+              selectedMonth={dateMonth}
+              selectedYear={dateYear}
+              goToPreviousMonth={decrementMonth}
+              goToNextMonth={incrementMonth}
+              goToPreviousYear={decrementYear}
+              goToNextYear={incrementYear}
               toggleMonthView={toggleMonthView}
               toggleYearView={toggleYearView}
             />
