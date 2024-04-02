089bc71bafc155403fc5ad34f1e8114541b9d312
Ben Ojha <ben@beforeyoubid.com.au>
2024-04-02 14:48:55 +1100

    Added react quill for text editor

---
+++ src/components/RichTextEditor.tsx
@@ -0,0 +1,96 @@
+import { memo, useCallback, useEffect, useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import { styled, Theme, useTheme } from '@mui/material';
+import { Flex } from './Flex';
+import { TextFieldErrorLabel, TextFieldHint, TextFieldLabel } from './TextInput/Labels';
+
+export type RichTextEditorProps = {
+  value: string;
+  label: string;
+  onChange: (value: string) => void;
+  required?: boolean;
+  isOptional?: boolean;
+  errorText?: string;
+  helperText?: string;
+  height?: number;
+  fullWidth?: boolean;
+};
+
+const modules = {
+  toolbar: [
+    ['bold', 'italic', 'underline'],
+    [{ align: ['right', 'center', 'justify'] }],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    ['link'],
+  ],
+};
+const formats = ['header', 'bold', 'italic', 'underline', 'list', 'bullet', 'link', 'image', 'align'];
+
+export const RichTextEditor = (props: RichTextEditorProps) => {
+  const {
+    value,
+    label,
+    onChange,
+    required = true,
+    errorText,
+    helperText,
+    isOptional = false,
+    height,
+    fullWidth = true,
+  } = props;
+
+  const handleChange = useCallback(
+    (value: string) => {
+      onChange(value);
+    },
+    [props]
+  );
+  const theme = useTheme();
+  return (
+    <Flex direction="column" gap={4} width={fullWidth ? '100%' : undefined}>
+      <TextFieldLabel labelText={label} required={required} isOptional={isOptional} />
+      {helperText && <TextFieldHint hintText={helperText} />}
+      {errorText && <TextFieldErrorLabel errorText={errorText} />}
+      <RichTextEditorComponent
+        modules={modules}
+        formats={formats}
+        value={value}
+        onChange={handleChange}
+        height={height}
+        errorText={errorText}
+        themeData={theme}
+      />
+    </Flex>
+  );
+};
+
+const RichTextEditorComponent = styled(ReactQuill)<{ themeData: Theme; height?: number; errorText?: string }>`
+  width: 100%;
+  border: 1px solid
+    ${({ errorText, themeData }) => (errorText ? themeData.palette.colors.error60 : themeData.palette.colors.dark45)};
+  border-radius: 4px;
+  background-color: ${({ errorText, themeData }) =>
+    errorText ? themeData.palette.colors.errorL1 : themeData.palette.colors.lightWhite};
+  theme: snow;
+
+  .ql-editor {
+    min-height: 200px;
+    height: ${({ height }) => height ?? '200'}px;
+    color: ${({ themeData }) => themeData.palette.colors.dark90};
+    font-family: 'Avenir-Medium';
+    font-weight: 400;
+
+    font-size: 16px;
+    border-top: 1px solid ${({ themeData }) => themeData.palette.colors.dark15};
+  }
+
+  .ql-container {
+    border: none !important;
+  }
+
+  .ql-toolbar {
+    border: none !important;
+  }
+`;
+export default memo(RichTextEditor);
--- src/stories/RichTextInput.stories.tsx
+++ src/stories/RichTextInput.stories.tsx
@@ -1,24 +1,19 @@
-import { RichTextInput } from '../components';
+import { RichTextEditor } from '../components';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
-const meta: Meta<typeof RichTextInput> = {
-  component: RichTextInput,
-  title: 'Input/RichTextInput',
+const meta: Meta<typeof RichTextEditor> = {
+  component: RichTextEditor,
+  title: 'Input/RichTextEditor',
 };
 
 export default meta;
-type Story = StoryObj<typeof RichTextInput>;
+type Story = StoryObj<typeof RichTextEditor>;
 
 export const Active: Story = {
   args: {
     label: 'Enter company detail',
-    placeholder: 'Placeholder',
     helperText: 'Please enter your company details',
-    minRows: 3,
-    disabled: false,
-    maxCharacter: 50,
-    hideTextCount: false,
     value:
       'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
   },
@@ -26,27 +21,8 @@ export const Active: Story = {
 export const Error: Story = {
   args: {
     label: 'Enter company detail',
-    placeholder: 'Placeholder',
     helperText: 'Please enter your company details',
     errorText: 'Error Text',
-    minRows: 3,
-    disabled: false,
-    maxCharacter: 50,
-    hideTextCount: false,
-    value:
-      'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
-  },
-};
-
-export const Disabled: Story = {
-  args: {
-    label: 'Enter company detail',
-    placeholder: 'Placeholder',
-    helperText: 'Please enter your company details',
-    minRows: 3,
-    disabled: true,
-    maxCharacter: 50,
-    hideTextCount: false,
     value:
       'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
   },
